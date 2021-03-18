7f169f76414ba42e9e04d67a507f1742bbeef4a2
Shin <ksshin21@gmail.com>
2021-03-18 14:40:17 +0900

    print stars in right triangle style
    오른쪽 직각삼각형 모양으로 별찍기

---
+++ 08 star_right_triangle.py
@@ -0,0 +1,11 @@
+# 8일차 
+#별찍기-오른쪽직각삼각형 1차시도 (KS)
+while True:
+    n = int(input('별의 개수: '))
+    if n == 0:
+        print('***** Bye~! *****')
+        break;
+    for i in range(n):
+        print(' '*(n-i+1), '*'*(i+1), end='')
+        print('')
+        
