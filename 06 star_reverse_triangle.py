60e0dc0099da6b272a604efa166a5bc67827c566
Shin <ksshin21@gmail.com>
2021-03-18 14:39:11 +0900

    print stars in reverse triangle
    역직각삼각형 모양으로 별찍기

---
+++ 06 star_reverse_triangle.py
@@ -0,0 +1,11 @@
+# 6일차
+#별찍기-역직각삼각형 1차시도 (KS)
+while True:
+    n = int(input('별의 개수: '))
+    if n == 0:
+        print("***** Bye~! *****")
+        break
+    while n > 0:
+        print('*'*n, end='')
+        print('')
+        n -= 1
